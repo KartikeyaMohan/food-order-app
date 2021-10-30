58bc55b98905b765aa4da79f959108a7953a8413
Kartikeya Mohan <kartikeyamohan@gmail.com>
2021-10-30 22:36:36 +0530

    Added header modules

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import { Fragment } from 'react';
+import HeaderCartButton from './HeaderCartButton.js'
+import mealsImage from '../../assests/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>Food Meals</h1>
+            <HeaderCartButton/>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt='A table full of food'/>
+        </div>
+    </Fragment>
+};
+
+export default Header;
