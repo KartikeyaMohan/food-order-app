c77a7f5885536fa899ffa6f3d811ab0f1616da2e
Kartikeya Mohan <kartikeyamohan@gmail.com>
2021-10-31 21:15:44 +0530

    Updated implementations for cart on static data

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,37 @@
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
-    return <button className = {classes.button}>
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+    const cartCtx = useContext(CartContext);
+    const { items } = cartCtx;
+    const numCartItems = 
+        items.reduce((currentNumber, item) => {
+            return currentNumber + item.amount;
+        }, 0);
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+            return;
+        }
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(true)   
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [items]);
+
+    return <button className = {btnClasses} onClick={props.onClick}>
         <span className = {classes.icon}>
             <CartIcon/>
         </span>
         <span>Your Cart</span>
-        <span className = {classes.badge}>3</span>
+        <span className = {classes.badge}>{numCartItems}</span>
     </button>
 };
 
